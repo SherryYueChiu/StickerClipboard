4a1b2057d69ae70316988f34e3c93e39c9e83fdc
Sherry <sherryyue.c@protonmail.com>
2024-09-09 23:25:32 +0800

    提升部份貼圖畫質

--- crower/stickerDownload.py
+++ crower/stickerDownload.py
@@ -1,15 +1,18 @@
 import requests
 import os
 
-startId=671996678
-endId=671996717
-folderName='ㄇㄚˊ幾兔ー塗鴉貼圖！'
+startId=124819206
+endId=124819229
+folderName='ㄇㄚˊ幾兔-賣萌大作戰'
 
 os.makedirs('./allSticker/'+folderName+'/',exist_ok=True)
 
 for stickerId in range(startId,endId):
     stickerId=str(stickerId)
-    url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/android/sticker.png'
+    # low resolution
+    # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/android/sticker.png'
+    # high resolution
+    url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker@2x.png'
     r=requests.get(url)
     with open('./allSticker/'+folderName+'/'+stickerId+'.png','wb') as f:
         f.write(r.content)
