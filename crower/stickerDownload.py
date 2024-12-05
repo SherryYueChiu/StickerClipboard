95186025e419b196d327e7f143ed869ec67829d2
Sherry <sherryyue.c@protonmail.com>
2024-12-06 02:11:17 +0800

    更新貼圖成動態圖

--- crower/stickerDownload.py
+++ crower/stickerDownload.py
@@ -1,9 +1,9 @@
 import requests
 import os
 
-startId=124819206
-endId=124819229
-folderName='ㄇㄚˊ幾兔-賣萌大作戰'
+startId=5865656
+endId=5865679
+folderName='ㄇㄚˊ幾兔（動幾來）'
 
 os.makedirs('./allSticker/'+folderName+'/',exist_ok=True)
 
@@ -12,7 +12,9 @@ for stickerId in range(startId,endId):
     # low resolution
     # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/android/sticker.png'
     # high resolution
-    url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker@2x.png'
+    # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker@2x.png'
+    # animation
+    url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker_animation@2x.png'
     r=requests.get(url)
     with open('./allSticker/'+folderName+'/'+stickerId+'.png','wb') as f:
         f.write(r.content)
