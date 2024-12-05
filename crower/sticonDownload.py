4b2a473106e943215291336cf59ffc8c03503b98
Sherry <sherryyue.c@protonmail.com>
2024-12-06 02:48:39 +0800

    新增表情貼

--- crower/sticonDownload.py
+++ crower/sticonDownload.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-folderName='cutekiwi move Emoji'
-sticonId='65484c048e943e070d985dbe'
+folderName='貓貓蟲-咖波 表情貼'
+sticonId='5ad6f009040ab14a7b11d10a'
 startId=1
 endId=40
 
@@ -11,9 +11,9 @@ os.makedirs('./allSticker/'+folderName+'/',exist_ok=True)
 for stickerId in range(startId,endId):
     stickerId=str(stickerId)
     # static
-    # url='https://stickershop.line-scdn.net/sticonshop/v1/sticon/'+sticonId+'/iPhone/'+stickerId.zfill(3)+'.png'
+    url='https://stickershop.line-scdn.net/sticonshop/v1/sticon/'+sticonId+'/iPhone/'+stickerId.zfill(3)+'.png'
     # animation
-    url='https://stickershop.line-scdn.net/sticonshop/v1/sticon/'+sticonId+'/iPhone/'+stickerId.zfill(3)+'_animation.png'
+    # url='https://stickershop.line-scdn.net/sticonshop/v1/sticon/'+sticonId+'/iPhone/'+stickerId.zfill(3)+'_animation.png'
     r=requests.get(url)
     with open('./allSticker/'+folderName+'/'+stickerId+'.png','wb') as f:
         f.write(r.content)
