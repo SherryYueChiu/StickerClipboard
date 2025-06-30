e55a51cfaca1fe632eefb74096fa0351ada76cc6
Sherry <sherryyue.c@protonmail.com>
2025-06-30 18:20:55 +0800

    新增貼圖

--- crower/stickerDownload.py
+++ crower/stickerDownload.py
@@ -1,9 +1,9 @@
 import requests
 import os
 
-startId=17093786
-endId=17093809
-folderName='ㄇㄚˊ幾兔（上班族篇）'
+startId=780332969
+endId=780333008
+folderName='ㄇㄚˊ幾兔－奇形怪狀篇？！'
 
 os.makedirs('./public/allSticker/'+folderName+'/',exist_ok=True)
 
@@ -12,9 +12,9 @@ for stickerId in range(startId,endId+1):
     # low resolution
     # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/android/sticker.png'
     # high resolution
-    # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker@2x.png'
+    url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker@2x.png'
     # animation
-    url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker_animation@2x.png'
+    # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker_animation@2x.png'
     # message sticker
     # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/base/plus/sticker@2x.png'
     r=requests.get(url)
