02455d359ebb4790092b24989ef30cdeccb79731
Sherry <sherryyue.c@protonmail.com>
2026-01-29 14:11:25 +0800

    更新動態貼圖

--- crower/stickerDownload.py
+++ crower/stickerDownload.py
@@ -12,9 +12,9 @@ for stickerId in range(startId,endId+1):
     # low resolution
     # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/android/sticker.png'
     # high resolution
-    url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker@2x.png'
+    # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker@2x.png'
     # animation
-    # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/sticker_animation@2x.png'
+    # url='https://stickershop.line-scdn.net/sticonshop/v1/sticon/616cd9d824a8ed1d418b28cd/iPhone/040_animation.png'
     # message sticker
     # url='https://stickershop.line-scdn.net/stickershop/v1/sticker/'+stickerId+'/iPhone/base/plus/sticker@2x.png'
     r=requests.get(url)
